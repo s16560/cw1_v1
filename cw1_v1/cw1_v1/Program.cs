a1a93abec99acb15ff8c10f0ef3b6474789b87a9
Michal <michalpietrzak@o2.pl>
2020-03-28 23:40:23 +0100

    Dodany projekt

---
+++ cw1_v1/cw1_v1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cw1_v1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
